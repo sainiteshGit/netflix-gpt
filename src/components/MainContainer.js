725019acc6a04a05af1030f274382556b66416bc
sainiteshGit <nitesh_4u@yahoo.com>
2023-09-01 21:15:40 -0400

    update the video info

---
+++ src/components/MainContainer.js
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import VideoTitle from "./VideoTitle";
+import VideoBackground from "./VideoBackground";
+
+const MainContainer = () => {
+
+    const movies = useSelector((store)=>store.movies?.addNowPlayingMovies);
+    
+    if(!movies)return;
+    const mainMovie = movies[0];
+    console.log(mainMovie);
+    const {original_title,overview} = mainMovie;
+
+    return (
+     <div>
+        <VideoTitle title={original_title} overview={overview}/>
+        <VideoBackground/>
+    </div>
+    );
+  };
+  
+  export default MainContainer;
+  
---
+++ src/components/SecondaryContainer.js
@@ -0,0 +1,11 @@
+const SecondaryContainer = () => {
+  
+    return (
+      <div>
+        secondary container
+      </div>
+    );
+  };
+  
+  export default SecondaryContainer;
+  
